92eb6696b9316bf12d289625e2f8ba438af125bb
Daan Schipper <2778477+daanschipper@users.noreply.github.com>
2025-02-05 10:31:52 +0100

    use direct reference for proto message

--- internal/converter/schema/schema.go
+++ internal/converter/schema/schema.go
@@ -100,10 +100,12 @@ func FieldToSchema(opts options.Options, parent *base.SchemaProxy, tt protorefle
 		return base.CreateSchemaProxy(s)
 	} else {
 		switch tt.Kind() {
-		case protoreflect.MessageKind, protoreflect.EnumKind:
+		case protoreflect.EnumKind:
 			msg := ScalarFieldToSchema(opts, parent, tt, false)
 			msg.AllOf = append(msg.AllOf, ReferenceFieldToSchema(opts, parent, tt))
 			return base.CreateSchemaProxy(msg)
+		case protoreflect.MessageKind:
+			return ReferenceFieldToSchema(opts, parent, tt)
 		}
 
 		return base.CreateSchemaProxy(ScalarFieldToSchema(opts, parent, tt, false))
